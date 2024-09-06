3d99c4025a94bd42ab1a2248cb1914b242189e55
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-09-06 16:10:48 +0200

    fix challenge 4

--- index.js
+++ index.js
@@ -5,17 +5,14 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(`Preferred type of Monster Energy drink: ${userPreference}`);
+console.log(`Favourite type of Monster Energy drink: ${userPreference ? userPreference: 'Regular'}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Any number of cans
-    if (cansLeft >= 5) {
-        console.log(`We're stocked!🥫`);
-    } else 
-        console.log(`Time to restock`);
+let cansLeft = 10; // Any number of cans
+    console.log(cansLeft < 5 ? "Time to restock!" : "We're stocked!🥫");
 
 
 // #### Challenge 3: Workout Intensity
@@ -23,7 +20,7 @@ let cansLeft = 3; // Any number of cans
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+    console.log(heartRate < 100 ? "Boost needed!" : "Energy levels are high!");
 
 
 // #### Challenge 4: Temperature Suitability
@@ -31,7 +28,9 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+    console.log(currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!") 
+
+   
 
 
 // #### Challenge 5: Late Night Coding Session
