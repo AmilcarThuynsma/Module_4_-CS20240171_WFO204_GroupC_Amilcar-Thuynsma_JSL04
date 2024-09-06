f7dac77750eab0839758e6af413afc64f7b88319
Amilcar Thuynsma <amilcarthuy@gmail.com>
2024-09-06 05:25:10 +0200

    add relational opertators to challenge 2

--- index.js
+++ index.js
@@ -5,14 +5,17 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(`Preferred type of Monster Energy drink: ${userPreference}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+    if (cansLeft >= 5) {
+        console.log(`We're stocked!🥫`);
+    } else 
+        console.log(`Time to restock`);
 
 
 // #### Challenge 3: Workout Intensity
